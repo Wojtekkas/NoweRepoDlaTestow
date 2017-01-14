d7fe4d5d35c3daca996e67d9f0f7fef42c63afef
wojtekkas <kas@post.pl>
2017-01-14 09:08:02 +0100

    add extra input, add numbers to patients

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $(function () {
     var patientsList = [
         { firstName: "Jan", lastName: "Kowalski" },
-        { firstName: "Jan", lastName: "Kowalski" },
-        { firstName: "Jan", lastName: "Kowalski" },
-        { firstName: "Jan", lastName: "Kowalski" },
+        { firstName: "Jan", lastName: "Kowalski2" },
+        { firstName: "Jan", lastName: "Kowalski3" },
+        { firstName: "Jan", lastName: "Kowalski4" },
         { firstName: "Adam", lastName: "Nowak" },
-        { firstName: "Adam", lastName: "Nowak" },
-        { firstName: "Adam", lastName: "Nowak" },
-        { firstName: "Adam", lastName: "Nowak" },
+        { firstName: "Adam", lastName: "Nowak2" },
+        { firstName: "Adam", lastName: "Nowak3" },
+        { firstName: "Adam", lastName: "Nowak4" },
         { firstName: "Andrzej", lastName: "Kielcz" },
-        { firstName: "Andrzej", lastName: "Kielcz" },
-        { firstName: "Andrzej", lastName: "Kielcz" },
-        { firstName: "Andrzej", lastName: "Kielcz" }
+        { firstName: "Andrzej", lastName: "Kielcz2" },
+        { firstName: "Andrzej", lastName: "Kielcz3" },
+        { firstName: "Andrzej", lastName: "Kielcz4" }
     ];
 
     patientsList.forEach(function (patient) {
